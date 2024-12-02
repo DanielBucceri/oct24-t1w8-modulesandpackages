254ecc206a6dcd0e44741c46f48c51387b9e2588
Daniel <gcas012125@coderacademy.edu.au>
2024-12-02 20:59:53 +1100

    managing dependencies. venv

---
+++ main.py
@@ -0,0 +1,9 @@
+from color50 import rgb, constants
+
+# Define a color using RGB values
+my_color = rgb(128, 0, 128)  # Purple
+
+# Print colored text
+print(my_color + "Hello, World!")
+
+print("hellow world")
---
+++ my_module.py
@@ -0,0 +1,14 @@
+
+
+__greeting = 'Hello world'
+
+nums  = [2, 1, 3]
+
+person = {
+    'name': "James",
+    'age': "999",
+}
+
+def foo(person):
+    print(f'{person.get('name')} is {person.get('age')} years old')
+    
